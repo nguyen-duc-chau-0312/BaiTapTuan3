3ede815eceb350b6a93cd06729ab6b8951362fa2
nguyen-duc-chau-0312 <nguyenducchau.1997@gmail.com>
2022-05-27 11:12:43 +0700

    update file sendata

---
+++ BaiTapTuan3/BaiTapTuan3/Data/SendData.swift
@@ -0,0 +1,12 @@
+//
+//  SendData.swift
+//  BaiTapTuan3
+//
+//  Created by Nguyen Duc Chau on 27/05/2022.
+//
+
+import UIKit
+
+protocol SendData {
+    func getButton(button: [UIButton?: String])
+}
